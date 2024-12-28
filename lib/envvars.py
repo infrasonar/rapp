003b44bb6c2acf644ff36b73719b5e4b279bdd7d
Jeroen van der Heijden <jeroen@cesbit.com>
2024-12-28 10:20:34 +0100

    COMPOSE_PROJECT_NAME

--- lib/envvars.py
+++ lib/envvars.py
@@ -7,14 +7,14 @@ ENV_FILE = os.getenv('ENV_FILE', '/docker/.env')
 CONFIG_FILE = os.getenv('CONFIG_FILE', '/config/infrasonar.yaml')
 COMPOSE_PATH = os.path.dirname(COMPOSE_FILE)
 USE_DEVELOPMENT = bool(int(os.getenv('USE_DEVELOPMENT', '0')))
-COMPOSE_PROJECT_NAME = os.getenv('COMPOSE_PROJECT_NAME', '')
-if COMPOSE_PROJECT_NAME:
+PROJECT_NAME = os.getenv('PROJECT_NAME', '')
+if PROJECT_NAME:
     # There is a downside in setting the project name: Docker compose will
     # work, even when the path where the docker compose file is mounted does
     # not reflect the path on the host. But, when used, containers will
     # be re-created as dockers detects this as a changes, resulting in
     # potentially unwanted restarts for containers.
-    #  An example COMPOSE_PROJECT_NAME = infrasonar
-    COMPOSE_CMD = f'docker compose -p {COMPOSE_PROJECT_NAME} --progress plain'
+    #  An example PROJECT_NAME = infrasonar
+    COMPOSE_CMD = f'docker compose -p {PROJECT_NAME} --progress plain'
 else:
     COMPOSE_CMD = 'docker compose --progress plain'
